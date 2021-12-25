d3888e000cd37caab9049163f8cdb032da360c8e
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2021-12-26 00:48:21 +0200

    Remove redundant elements from login page. Add dashboard and content template pages. Add used AdminLTE template folder.

---
+++ frontend/pos-admin-php/resources/views/includes/foot.blade.php
@@ -0,0 +1,22 @@
+        <!-- Main Footer -->
+        <footer class="main-footer">
+            <!-- To the right -->
+            <div class="float-right d-none d-sm-inline">
+                InventoryPOS v1.0
+            </div>
+            <!-- Default to the left -->
+            &copy; <?= date('Y') ?> All rights reserved.
+        </footer>
+        </div>
+        <!-- ./wrapper -->
+
+        <!-- REQUIRED SCRIPTS -->
+
+        <!-- jQuery -->
+        <script src="plugins/jquery/jquery.min.js"></script>
+        <!-- Bootstrap 4 -->
+        <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- AdminLTE App -->
+        <script src="dist/js/adminlte.min.js"></script>
+    </body>
+</html>
