362405f8fa91e5b7294b9c06a84e4e0d9867ddc4
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2022-01-11 00:18:37 +0200

    Add other test users to test_users.sql

--- backend/pos-system-admin-java-quarkus/test_users.sql
+++ backend/pos-system-admin-java-quarkus/test_users.sql
@@ -1,4 +1,9 @@
--- Add test users
+-- USER and ADMIN test users
 insert into pos_users(username, email, password, salt, role) values('admin', 'admin@test.com', '123123', '123', 'ADMIN');
 insert into pos_users(username, email, password, salt, role) values('user', 'user@test.com', '123123', '123', 'USER');
 
+-- Other test users
+insert into pos_users(username, email, password, salt, role) values('test1', 'test1@test.com', '123123', '123', 'USER');
+insert into pos_users(username, email, password, salt, role) values('test2', 'test2@test.com', '123123', '123', 'USER');
+insert into pos_users(username, email, password, salt, role) values('test3', 'test3@test.com', '123123', '123', 'USER');
+insert into pos_users(username, email, password, salt, role) values('test4', 'test4@test.com', '123123', '123', 'USER');
