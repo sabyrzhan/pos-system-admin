651d81f2db6703874466a34848c5035c09dcb94e
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2022-02-05 21:09:51 +0200

    backend: add OrdersResource#getList

--- backend/pos-system-admin-java-quarkus/src/main/java/kz/sabyrzhan/repositories/OrderRepository.java
+++ backend/pos-system-admin-java-quarkus/src/main/java/kz/sabyrzhan/repositories/OrderRepository.java
@@ -1,10 +1,16 @@
 package kz.sabyrzhan.repositories;
 
 import io.quarkus.hibernate.reactive.panache.PanacheRepositoryBase;
+import io.quarkus.panache.common.Sort;
+import io.smallrye.mutiny.Multi;
 import kz.sabyrzhan.entities.OrderEntity;
 
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class OrderRepository implements PanacheRepositoryBase<OrderEntity, Integer> {
+    public Multi<OrderEntity> list(int page, int sizePerPage) {
+        return OrderEntity.findAll(Sort.descending("created"))
+                .page(page - 1, sizePerPage).stream();
+    }
 }
--- backend/pos-system-admin-java-quarkus/src/main/java/kz/sabyrzhan/resources/OrdersResource.java
+++ backend/pos-system-admin-java-quarkus/src/main/java/kz/sabyrzhan/resources/OrdersResource.java
@@ -1,15 +1,18 @@
 package kz.sabyrzhan.resources;
 
+import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 import kz.sabyrzhan.entities.OrderEntity;
+import kz.sabyrzhan.repositories.OrderRepository;
 import kz.sabyrzhan.services.OrderService;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.QueryParam;
 
 @Path("/api/v1/orders")
 @ApplicationScoped
@@ -18,8 +21,20 @@ public class OrdersResource {
     @Inject
     OrderService orderService;
 
+    @Inject
+    OrderRepository orderRepository;
+
     @POST
     public Uni<OrderEntity> createOrder(OrderEntity order) {
         return orderService.createOrder(order);
+    }
+
+    @GET
+    public Multi<OrderEntity> getList(@QueryParam("page") int page) {
+        if (page == 0) {
+            page = 1;
+        }
+
+        return orderRepository.list(page, 30);
     }
 }
