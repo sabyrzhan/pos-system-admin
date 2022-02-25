6443ffae7fb6ed61d261620f42a8e07ebe4235bc
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2022-02-26 01:34:27 +0200

    - backend: add getOrders and createOrder load test scenarios

--- backend/load_test/run.sh
+++ backend/load_test/run.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-jmeter -n -t $PWD/LoadTest_getProducts.jmx -f  -l $PWD/jmeter.log -e -o $PWD/jmeter_dash
+test=$1
+echo $1
+case $test in
+  products)
+    jmeter -n -t $PWD/LoadTest_getProducts.jmx -f  -l $PWD/jmeter.log -e -o $PWD/jmeter_dash
+    ;;
+  orders)
+    jmeter -n -t $PWD/LoadTest_getOrders.jmx -f  -l $PWD/jmeter.log -e -o $PWD/jmeter_dash
+    ;;
+  createorder)
+    jmeter -n -t $PWD/LoadTest_createOrder.jmx -f  -l $PWD/jmeter.log -e -o $PWD/jmeter_dash
+    ;;
+  *)
+    echo 'Invalid test name'
+    ;;
+esac
