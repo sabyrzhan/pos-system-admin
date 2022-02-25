bef8ae6d96271dece9b89456c738ff9dc39f8a4b
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2022-02-25 22:19:02 +0200

    update run.sh

--- backend/grafana_dashboard/run.sh
+++ backend/grafana_dashboard/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export POS_BACKEND_HOST=$(ifconfig en0 inet | grep inet | awk '{ print $2 }')
+export POS_BACKEND_HOST=$(ifconfig docker0 | grep inet | awk '{ print $2 }')
 docker-compose -f docker-compose-grafana.yml up
