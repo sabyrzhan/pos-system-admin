054ae815e55581f6d02ba6dd632ca20293415c34
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2022-02-27 15:57:07 +0200

    Add db slave nodes

--- backend/grafana_dashboard/run.sh
+++ backend/grafana_dashboard/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 export POS_BACKEND_HOST=192.168.1.142
 export POS_DB_HOST_MASTER=192.168.0.11
+export POS_DB_HOST_SLAVE1=192.168.0.12
+export POS_DB_HOST_SLAVE2=192.168.0.13
+export POS_DB_HOST_SLAVE3=192.168.0.14
 docker-compose -f docker-compose-grafana.yml up -d
