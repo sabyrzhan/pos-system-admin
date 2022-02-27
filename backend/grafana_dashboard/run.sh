9b8688e70203aacde9014a6bb945469e2168b2bd
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2022-02-27 16:40:24 +0200

    Add db monitoring docker-compose file

--- backend/grafana_dashboard/run.sh
+++ backend/grafana_dashboard/run.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-export POS_BACKEND_HOST=192.168.1.142
-export POS_DB_HOST_MASTER=192.168.0.11
-export POS_DB_HOST_SLAVE1=192.168.0.12
-export POS_DB_HOST_SLAVE2=192.168.0.13
-export POS_DB_HOST_SLAVE3=192.168.0.14
-docker-compose -f docker-compose-grafana.yml up -d
+action=$1
+case $action in 
+  grafana)
+    export POS_BACKEND_HOST=192.168.1.142
+    export POS_DB_HOST_MASTER=192.168.0.11
+    export POS_DB_HOST_SLAVE1=192.168.0.12
+    export POS_DB_HOST_SLAVE2=192.168.0.13
+    export POS_DB_HOST_SLAVE3=192.168.0.14
+    docker-compose -f docker-compose-grafana.yml up -d
+    ;;
+  monitoring)
+    docker-compose -f docker-compose-monitoring.yml up -d
+    ;;
+  *)
+    echo 'Invalid action'
+    ;;
+esac
