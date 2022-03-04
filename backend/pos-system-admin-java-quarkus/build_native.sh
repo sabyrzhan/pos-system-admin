ba93a59ad7ad9a1b294b707f09d19e37afa69929
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2022-03-05 00:36:46 +0200

    - backend: update build_native.sh to support build on m1

--- backend/pos-system-admin-java-quarkus/build_native.sh
+++ backend/pos-system-admin-java-quarkus/build_native.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 set -e
-./mvnw clean package \
-  -Pnative \
-  -DskipTests \
-  -Dquarkus.native.additional-build-args='--initialize-at-build-time=java.awt.color.ColorSpace$BuiltInSpace' \
-  -Dquarkus.native.container-build=true \
-  -Dquarkus.native.builder-image=quay.io/quarkus/ubi-quarkus-native-image:22.0-java17 \
-  -Dquarkus.native.native-image-xmx=6g \
-  -Dquarkus.native.container-runtime-options='-m=8g'
-
-docker-compose build
+target=$1
+case $target in
+  arm)
+    ./mvnw clean package \
+      -Pnative \
+      -DskipTests \
+      -Dquarkus.native.additional-build-args='--initialize-at-build-time=java.awt.color.ColorSpace$BuiltInSpace' \
+      -Dquarkus.container-image.tag=pos-system-backend \
+      -Dquarkus.container-image.build=true \
+      -Dquarkus.native.native-image-xmx=6g
+    ;;
+  *)
+    ./mvnw clean package \
+      -Pnative \
+      -DskipTests \
+      -Dquarkus.native.additional-build-args='--initialize-at-build-time=java.awt.color.ColorSpace$BuiltInSpace' \
+      -Dquarkus.native.container-build=true \
+      -Dquarkus.container-image.tag=latest \
+      -Dquarkus.container-image.build=true \
+      -Dquarkus.native.builder-image=quay.io/quarkus/ubi-quarkus-native-image:22.0-java17 \
+      -Dquarkus.native.native-image-xmx=6g \
+      -Dquarkus.native.container-runtime-options='-m=8g'
+      #docker-compose build
+    ;;
+esac
