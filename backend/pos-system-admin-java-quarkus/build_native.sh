88c795afbae0e99d6606b2cd9183427e4656fb70
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2022-03-05 03:12:14 +0200

    - backend: add container-image-docker extension to build native container image.

--- backend/pos-system-admin-java-quarkus/build_native.sh
+++ backend/pos-system-admin-java-quarkus/build_native.sh
@@ -7,21 +7,28 @@ case $target in
       -Pnative \
       -DskipTests \
       -Dquarkus.native.additional-build-args='--initialize-at-build-time=java.awt.color.ColorSpace$BuiltInSpace' \
-      -Dquarkus.container-image.tag=pos-system-backend \
       -Dquarkus.container-image.build=true \
       -Dquarkus.native.native-image-xmx=6g
+    ;;
+  container)
+    ./mvnw package \
+          -DskipTests \
+          -Dquarkus.container-image.build=true \
+          -Dquarkus.container-image.group=\
+          -Dquarkus.container-image.tag=latest
     ;;
   *)
     ./mvnw clean package \
       -Pnative \
       -DskipTests \
       -Dquarkus.native.additional-build-args='--initialize-at-build-time=java.awt.color.ColorSpace$BuiltInSpace' \
-      -Dquarkus.native.container-build=true \
+      -Dquarkus.container-image.build=true
+      -Dquarkus.container-image.group=\
       -Dquarkus.container-image.tag=latest \
-      -Dquarkus.container-image.build=true \
+      -Dquarkus.native.container-build=true \
       -Dquarkus.native.builder-image=quay.io/quarkus/ubi-quarkus-native-image:22.0-java17 \
-      -Dquarkus.native.native-image-xmx=6g \
-      -Dquarkus.native.container-runtime-options='-m=8g'
+      -Dquarkus.native.native-image-xmx=14g \
+      -Dquarkus.native.container-runtime-options='-m=16g'
       #docker-compose build
     ;;
 esac
